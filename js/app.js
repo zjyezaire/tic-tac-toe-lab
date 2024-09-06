2088df01831b434adc4ac535ebd10a251790535d
zjyezaire <zaire.elleby@gmail.com>
2024-09-06 12:54:44 -0400

    nearly complete

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const squareEls = document.querySelectorAll("sqr");
+const squareEls = document.querySelectorAll('.sqr');
 console.log(squareEls);
 
+const messageEl = document.querySelector('#message');
+console.log(messageEl)
+
+let board;
+let turn;
+let winner;
+let tie;
+
+/* Another way to write above stateemnt by using getElementByID
 const messageEl = document.getElementById("message");
 console.log(messageEl);
+*/
 
 const winningCombos = [
   [0, 1, 2],
@@ -16,42 +26,42 @@ const winningCombos = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-
-/*---------------------------- Variables (state) ----------------------------*/
-let board;
-let turn;
-let winner;
-let tie;
-
-/*------------------------ Cached Element References ------------------------*/
-
-/*-------------------------------- Functions --------------------------------*/
-function updateBoard() {
-  board.forEach((value, index) => {
-    squareEls[index].textContent = value;
-  });
+const placePiece = (index) => {
+    board[index] = turn;
 }
 
-function updateMessage() {
-  if (!winner && !tie) {
-    messageEl.textContent = `Turn: ${turn}`;
-  } else if (!winner && tie) {
-    messageEl.textContent = "It's a tie!";
-  } else {
-    messageEl.textContent = `Congratulations ${
-      turn === "X" ? "O" : "X"
-    }! You won!`;
-  }
+
+const handleClick = e => {
+    if (winner === true) return 
+        console.log([...e.target.classList].includes('sqr'))
+
+        if (e.target.classList.contains('sqr')) {
+            const squareIndex = e.target.id 
+            if(board[squareIndex] === 'x' || board[squareIndex] === 'o') {
+                return
+            }
+
+            placePiece(squareIndex)
+        }
+        return;
 }
 
-function render() {
-  updateBoard;
-  updateMessage;
+const checkForWinner = () => {
+    for(let i = 0; i < winningCombos.length; i++) {
+        for( let j = 0; j < winningCOmbos[i]; j++) {
+            console.log(board[j])
+        }
+        console.log(winningCombos[i])
+    }
 }
-function init() {
-  board = ["x", "", "", "", "", "", "", "", ""];
+
+document.querySelector('.board'). 
+addEventListener('click', handleClick)
+
+const init =() => {
+  board = ["", "", "", "", "", "", "", "", ""];
   console.log("The game has started");
-  turn = "X";
+  turn = 'X';
   winner = false;
   tie = false;
   render();
@@ -59,6 +69,40 @@ function init() {
 
 init();
 
+
+
+
+function updateBoard() {
+  board.forEach((square, squareIndex) => {
+    squareEls[squareIndex].textContent = square;
+  });
+}
+
+function updateMessage() {
+  if (!winner && !tie) {
+    messageEl.tektContent = turn;
+  } else if (!winner && tie) {
+    messageEl.textContent = "It's a tie!";
+  } else {
+    messageEl.textContent = '${turn} is the winner!'
+  }
+    
+   
+  }
+
+
+function render() {
+  updateBoard();
+  updateMessage();
+  placePiece(6);
+  console.log(board);
+}
+
+
+
+window.onload = () => {
+    init()
+}
 /*
 function checkForWinner() {
     winningCombos.forEach((combo)   =>)
@@ -69,16 +113,37 @@ function checkForWinner() {
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+
+
+
+
 squareEls.forEach((square) => {
-  square.addEventListener("click", handleSquareClick);
+  square.addEventListener("click", handleClick);
 });
 
-// Handle click events on the squares
-function handleSquareClick(evt) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function handleClick(evt) {
   const index = parseInt(evt.target.id);
   if (board[index] || winner) return;
   board[index] = turn;
   turn = turn === "X" ? "O" : "X";
   // Check for winner or tie logic would go here
   render();
-}
+
+  */
